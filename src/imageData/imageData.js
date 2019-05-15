dbcb3bb77e6fc7ea78133159cef050a89ca7e77e
jun <wanghaijun04@gmail.com>
2019-05-15 20:45:53 +1000

    set image info for sliding

---
+++ src/imageData/imageData.js
@@ -0,0 +1,46 @@
+import icon from '../assets/image/adam-muise-slider.jpg'
+import book from '../assets/image/annie-spratt-slider.jpg'
+import portrait from '../assets/image/gambler-slider.jpg'
+import film from '../assets/image/jakob-owens-slider.jpg'
+import room from '../assets/image/joseph-morris-slider.jpg'
+import kid from '../assets/image/josh-applegate-slider.jpg'
+
+const data =[
+  {
+    id:1,
+    url:icon,
+    title:'icon',
+    content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, libero finibus posuere congue, quam sapien tristique arcu, ut ullamcorper mi magna ac tortor. Cras sollicitudin, tortor quis ultrices maximus, leo magna ultricies nisl, vel rutrum mauris augue sed nisl'
+  },
+  {
+    id:2,
+    url:book,
+    title:'book',
+    content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, libero finibus posuere congue, quam sapien tristique arcu, ut ullamcorper mi magna ac tortor. Cras sollicitudin, tortor quis ultrices maximus, leo magna ultricies nisl, vel rutrum mauris augue sed nisl'
+  },
+  {
+    id:3,
+    url:portrait,
+    title:'portrait',
+    content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, libero finibus posuere congue, quam sapien tristique arcu, ut ullamcorper mi magna ac tortor. Cras sollicitudin, tortor quis ultrices maximus, leo magna ultricies nisl, vel rutrum mauris augue sed nisl'
+  },
+  {
+    id:4,
+    url:film,
+    title:'film',
+    content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, libero finibus posuere congue, quam sapien tristique arcu, ut ullamcorper mi magna ac tortor. Cras sollicitudin, tortor quis ultrices maximus, leo magna ultricies nisl, vel rutrum mauris augue sed nisl'
+  },
+  {
+    id:5,
+    url:room,
+    title:'room',
+    content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, libero finibus posuere congue, quam sapien tristique arcu, ut ullamcorper mi magna ac tortor. Cras sollicitudin, tortor quis ultrices maximus, leo magna ultricies nisl, vel rutrum mauris augue sed nisl'
+  },
+  {
+    id:6,
+    url:kid,
+    title:'kid',
+    content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, libero finibus posuere congue, quam sapien tristique arcu, ut ullamcorper mi magna ac tortor. Cras sollicitudin, tortor quis ultrices maximus, leo magna ultricies nisl, vel rutrum mauris augue sed nisl'
+  },
+]
+export default data
